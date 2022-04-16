5843d660e4b26d730685a025664b1ea1f25730f6
wanilalits <wanilalits@gmail.com>
2022-04-16 23:41:29 +0530

    changed file

--- src/App.js
+++ src/App.js
@@ -51,4 +51,5 @@ export default App;
 npm i --save @fortawesome/fontawesome-svg-core
 npm i --save @fortawesome/free-solid-svg-icons
 npm i --save @fortawesome/free-regular-svg-icons
+npm i --save @fortawesome/react-fontawesome@latest
 */
--- src/Copmonents/Home.jsx
+++ src/Copmonents/Home.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer';
 import {Card,Button } from 'react-bootstrap';
 import Responsive from '../Responsive/Responsive';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import './Home.css';
 class Home extends Component {
     render() {
         return (
@@ -15,9 +15,10 @@ class Home extends Component {
          
 
 
-          <br/> <br/>
-<div style={{  display: 'inline-block' , marginLeft:'20px', }}>
-          <Card  border= 'warning'  style={{ width: '30rem' }}>
+         <div className='home_grand_parent'>
+         <div className='home_parents'>
+       
+        <Card  border= 'warning' className='home_child' style={{  }}>
           <Card.Header style={{  textAlign: 'center', fontSize:"30px", fontWeight:"500" }} >IoT</Card.Header>
   <Card.Body>
  
@@ -34,30 +35,48 @@ class Home extends Component {
 
   </Card.Body>
 </Card>
-</div>
-
-<div style={{  display: 'inline-block',  marginLeft:'20px', }}>
-<Card  border= 'warning'  style={{ width: '30rem' }}>
+        
+        
+ 
+        
+        
+        <Card  border= 'warning' className='home_child' style={{ width: '300px' }}>
           <Card.Header style={{  textAlign: 'center', fontSize:"30px", fontWeight:"500" }} >React Js FrontEnd</Card.Header>
   <Card.Body>
  
     <br/>
-    <Card.Subtitle className="success"> - Dashboard <br/> -Router <br/>- Responsive CSS </Card.Subtitle>
+    <Card.Subtitle className="success"> - Dashboard <br/>- Responsive CSS </Card.Subtitle>
     <br/>
     <Card.Text>
     C & C++, HTML, CSS, JS, React JS
    <br/>API, Postman,  
      <br/>Bootstrap, Router, Material ui, 
      <br/>
-     FontAwesome, .......
+     FontAwesome, <br/>
+     .
+      
      <br/>
-     <br/>
-   
+     .
      
     </Card.Text>
 
   </Card.Body>
 </Card>
+        
+        
+    
+
+         </div>
+
+         </div>
+          <div style={{  display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
+<div style={{  display: 'inline-block',     }}>
+    
+</div>
+
+<div style={{  display: 'inline-block', marginLeft:"0px",marginTop:"10px",}}>
+
+</div>
 </div>
           <br/><br/>.<br/>.<br/>.<br/>.<br/><br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.<br/>.
             
